9124f7412b2a6cdf918bf5f34c1e0e2bb931deec
KujouRinka <kujourinka@gmail.com>
2022-06-23 23:30:56 +0800

    feat: add buffer utility

--- common/buffer.cc
+++ common/buffer.cc
@@ -1,1 +1,80 @@
 #include "buffer.h"
+
+#include <algorithm>
+
+#include "connection/connection.h"
+
+BlockBuffer::~BlockBuffer() = default;
+
+ComposeBuffer::ComposeBuffer() = default;
+
+ComposeBuffer::ComposeBuffer(initializer_list<BlockBuffer *> blocks)
+    : _buffers(blocks) {}
+
+string ComposeBuffer::get(size_t size) {
+    string ret;
+    while (!_buffers.empty() && (ret = _buffers.front()->get(size)).empty()) {
+        delete _buffers.front();
+        _buffers.pop();
+        if (_buffers.empty())
+            break;
+    }
+    return ret;
+}
+
+void ComposeBuffer::appendBuffer(BlockBuffer *buffer) {
+    _buffers.push(buffer);
+}
+
+ResponseBlockBuffer::ResponseBlockBuffer(const Response &response)
+    : idx(0) {
+    // I guess the usual size of response is about 128 bytes
+    data.reserve(128);
+    data.append(response.version).append(" ")
+        .append(response.status).append(" ")
+        .append(response.reason).append("\r\n");
+    for_each(response.headers.begin(), response.headers.end(),
+             [&](auto &header) {
+                 data.append(header.first).append(": ")
+                     .append(header.second).append("\r\n");
+             });
+    data.append("\r\n");
+}
+
+string ResponseBlockBuffer::get(size_t size) {
+    string s = data.substr(idx, size);
+    idx += s.size();
+    return std::move(s);
+}
+
+FileIterBlockBuffer::FileIterBlockBuffer(ifstream ifs)
+    : fs(std::move(ifs)) {}
+
+// TODO:
+string FileIterBlockBuffer::get(size_t size) {
+    string ret;
+    ret.resize(size);
+    fs.read(ret.data(), size);
+    ret.resize(fs.gcount());
+    return ret;
+}
+
+FileDescriptorBlockBuffer::FileDescriptorBlockBuffer(int fd)
+    : _fd(fd) {}
+
+FileDescriptorBlockBuffer::~FileDescriptorBlockBuffer() {
+    close(_fd);
+}
+
+string FileDescriptorBlockBuffer::get(size_t size) {
+    string ret;
+    ret.resize(size);
+    auto read_size = read(_fd, ret.data(), size);
+    if (read_size == 0)
+        return "";
+    // TODO: what to do for error?
+    // else if (read_size == -1)
+    //     throw runtime_error("read error");
+    ret.resize(read_size);
+    return std::move(ret);
+}
--- common/buffer.h
+++ common/buffer.h
@@ -1,24 +1,74 @@
 #ifndef TINYSERVER_ASYNC_BUFFER_H
 #define TINYSERVER_ASYNC_BUFFER_H
 
-#include <unistd.h>
+#include <fstream>
+#include <queue>
+#include <utility>
 
-// intend to implement replaceable buffer
-/*
-class Buffer {
+using namespace std;
+
+class Response;
+
+using buffer_range = pair<char *, char *>;
+
+// ---------------------------------------------
+// Buffer interface
+class BlockBuffer {
 public:
-    ssize_t size() {
-        return 0;
-    }
-
-    ssize_t cap() {
-        return 0;
-    }
-
-    char *buffer() {
-        return nullptr;
-    }
+    virtual ~BlockBuffer();
+    virtual string get(size_t size) = 0;
 };
-*/
+
+// ---------------------------------------------
+// CompressedBuffer
+class ComposeBuffer : public BlockBuffer {
+public:
+    ComposeBuffer();
+    ComposeBuffer(initializer_list<BlockBuffer *> blocks);
+    string get(size_t size) override;
+    void appendBuffer(BlockBuffer *buffer);
+
+private:
+    // order in _buffers matters
+    queue<BlockBuffer *> _buffers;
+};
+
+// ---------------------------------------------
+// ResponseBlockBuffer
+class ResponseBufferTest;
+class ResponseBlockBuffer : public BlockBuffer {
+    friend class ResponseBufferTest;
+public:
+    explicit ResponseBlockBuffer(const Response &response);
+    string get(size_t size) override;
+
+private:
+    string::size_type idx;
+    string data;
+};
+
+// ---------------------------------------------
+// FileBlockBuffer
+// TODO:
+class FileIterBlockBuffer : public BlockBuffer {
+public:
+    explicit FileIterBlockBuffer(ifstream ifs);
+    string get(size_t size) override;
+
+private:
+    ifstream fs;
+};
+
+// ---------------------------------------------
+// FileDescriptorBlockBuffer
+class FileDescriptorBlockBuffer : public BlockBuffer {
+public:
+    explicit FileDescriptorBlockBuffer(int fd);
+    ~FileDescriptorBlockBuffer();
+    string get(size_t size) override;
+
+private:
+    int _fd;
+};
 
 #endif //TINYSERVER_ASYNC_BUFFER_H
