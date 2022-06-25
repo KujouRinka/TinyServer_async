d40710ecd8fd77db81c58cd76f103435d7d4b4c7
KujouRinka <kujourinka@gmail.com>
2022-06-25 17:38:57 +0800

    feat: add threadpool

--- threadpool/threadpool.cc
+++ threadpool/threadpool.cc
@@ -1,1 +1,40 @@
 #include "threadpool.h"
+
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+ThreadPool::ThreadPool(int max_threads, int max_queue_size)
+  : _max_queue_size(max_queue_size) {
+  if (max_threads <= 0 || max_queue_size <= 0) {
+    cerr << "invalid thread pool init parameter" << endl;
+    exit(0);
+  }
+  while (max_threads--) {
+    thread t(&ThreadPool::worker, this);
+    t.detach();
+  }
+  _running = true;
+}
+
+bool ThreadPool::appendTask(function<void()> task) {
+  lock_guard<mutex> g(_queue_lock);
+  if (_task_queue.size() >= _max_queue_size)
+    return false;
+  _task_queue.push(task);
+  _cv.notify_one();
+  return true;
+}
+
+void ThreadPool::worker() {
+  while (_running) {
+    unique_lock<mutex> lk(_queue_lock);
+    _cv.wait(lk, [this]() { return !_task_queue.empty(); });
+    auto task_fun = _task_queue.front();
+    _task_queue.pop();
+    lk.unlock();
+    if (task_fun != nullptr)
+      task_fun();
+  }
+}
--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -1,11 +1,27 @@
 #ifndef TINYSERVER_ASYNC_THREADPOOL_H
 #define TINYSERVER_ASYNC_THREADPOOL_H
 
+#include <queue>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+#include <functional>
+
+using namespace std;
+
 class ThreadPool {
  public:
-  ThreadPool(int max_threads, int max_queue_size);
+  ThreadPool(int max_threads = 16, int max_queue_size = 10000);
 
-  void addTask();
+  bool appendTask(function<void()> task);
+  void worker();
+
+ private:
+  bool _running;
+  int _max_queue_size;
+  mutex _queue_lock;
+  queue<function<void()>> _task_queue;
+  condition_variable _cv;
 };
 
 #endif //TINYSERVER_ASYNC_THREADPOOL_H
